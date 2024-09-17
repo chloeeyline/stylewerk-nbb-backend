ad9ba399343336cd83fb03a49345e783939a3667
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-09-17 13:52:49 +0200

    Added Local Symbol to Project File

--- ApiServer/ApiServer/Controllers/EntryOverviewController.cs
+++ ApiServer/ApiServer/Controllers/EntryOverviewController.cs
@@ -1,42 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
 using StyleWerk.NBB.Queries;
 using StyleWerk.NBB.Queries.Dto;
-using System.Text.Json;
 
 namespace StyleWerk.NBB.Controllers
 {
-    public class EntryOverviewController : Controller
-    {
-        private readonly EntryQueries _entryQueries;
+	public class EntryOverviewController : Controller
+	{
+		private readonly EntryQueries _entryQueries;
 
-        public EntryOverviewController(EntryQueries entryQueries)
-        {
-            _entryQueries = entryQueries;
-        }
+		public EntryOverviewController(EntryQueries entryQueries)
+		{
+			_entryQueries = entryQueries;
+		}
 
-        List<EntryViewModel> Entries { get; set; }
-        EntryContentViewModel Entry { get; set; }
+		List<EntryViewModel> Entries { get; set; }
+		EntryContentViewModel Entry { get; set; }
 
 
-        [HttpGet]
-        public IActionResult GetEntriesByUserId(Guid userId)
-        {
-            Entries = _entryQueries.LoadEntriesByUserId(userId);
-            return Ok(Entries);
-        }
+		[HttpGet]
+		public IActionResult GetEntriesByUserId(Guid userId)
+		{
+			Entries = _entryQueries.LoadEntriesByUserId(userId);
+			return Ok(Entries);
+		}
 
-        [HttpGet]
-        public IActionResult GetEntryContent(Guid userId)
-        {
-            Entry = _entryQueries.LoadEntryByUserId(userId);
-            return Ok(Entry);
-        }
+		[HttpGet]
+		public IActionResult GetEntryContent(Guid userId)
+		{
+			Entry = _entryQueries.LoadEntryByUserId(userId);
+			return Ok(Entry);
+		}
 
-        [HttpPost]
-        public IActionResult SaveEntry()
-        {
+		//[HttpPost]
+		//public IActionResult SaveEntry()
+		//{
 
-        }
+		//}
 
-    }
+	}
 }
