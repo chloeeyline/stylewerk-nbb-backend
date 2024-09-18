ccbb04effd38d68b2666ddb5b3b28e28e6fb8bf5
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-09-18 15:37:00 +0200

    Added TemplateOverview Models

--- ApiServer/ApiServer/Models/TemplateOverview.cs
+++ ApiServer/ApiServer/Models/TemplateOverview.cs
@@ -1,6 +1,9 @@
-﻿namespace StyleWerk.NBB.Models
-{
-	public class TemplateOverview
-	{
-	}
-}
+﻿using ChaosFox.Models;
+
+namespace StyleWerk.NBB.Models;
+
+
+public record TemplateOVerview(int Count, List<TemplateItem> Items) : PagingList<TemplateItem>(Count, Items);
+public record TemplateItem(Guid ID, string Name, string UserName, string Description, DateTime CreatedAt, DateTime LastUpdatedAt, ShareType Share);
+
+public record Model_FilterTemplate(string? Name, string? Username, string? Description, ShareType? Share, string[]? Tags);
