287213902a593b402d416a2114604eb5dda74024
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-09-20 08:45:01 +0200

    Added missing Tags and IsPublic Fields

---
+++ ApiServer/ApiServer/Migrations/20240920064429_AddedMissingFields.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StyleWerk.NBB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMissingFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string[]>(
+                name: "Tags",
+                table: "Structure_Template",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Structure_Entry",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Structure_Entry",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "Tags",
+                table: "Structure_Entry",
+                type: "text[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Structure_Template");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Structure_Entry");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Structure_Entry");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Structure_Entry",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
