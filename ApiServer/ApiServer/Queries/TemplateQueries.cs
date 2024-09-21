924f5a74fbf93e26ee45be43d05bb5e59c1a1cb6
schwammal666 <schwammal55@gmail.com>
2024-09-21 14:36:56 +0200

    Added User to TemplateQueries

--- ApiServer/ApiServer/Queries/TemplateQueries.cs
+++ ApiServer/ApiServer/Queries/TemplateQueries.cs
@@ -1,14 +1,20 @@
-﻿using StyleWerk.NBB.Database;
+﻿
+using StyleWerk.NBB.Authentication;
+using StyleWerk.NBB.Database;
 
 namespace StyleWerk.NBB.Queries
 {
     public class TemplateQueries
     {
         private readonly NbbContext _context;
+        private readonly ApplicationUser _user;
 
-        public TemplateQueries(NbbContext context)
+        public TemplateQueries(NbbContext context, ApplicationUser user)
         {
+            _user = user;
             _context = context;
         }
+
+
     }
 }
