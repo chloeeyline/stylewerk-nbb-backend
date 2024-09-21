205f08b233b42f7062f4c2f93ef1cde08741bfc2
schwammal666 <schwammal55@gmail.com>
2024-09-21 14:37:48 +0200

    EntryEditorController inherits from BaseController

---
+++ ApiServer/ApiServer/Controllers/EntryEditorController.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using StyleWerk.NBB.Database;
+using StyleWerk.NBB.Database.Structure;
+using StyleWerk.NBB.Database.User;
+using StyleWerk.NBB.Models;
+
+namespace StyleWerk.NBB.Controllers
+{
+    [ApiController, Route("EntryOverview")]
+    public class EntryEditorController : BaseController
+    {
+
+        public EntryEditorController(NbbContext db) : base(db)
+        {
+        }
+
+        [HttpPost(nameof(DeleteEntry))]
+        public IActionResult DeleteEntry(Guid entryId)
+        {
+            Structure_Entry? item = DB.Structure_Entry.FirstOrDefault(e => e.ID == entryId);
+            if (item != null) DB.Structure_Entry.Remove(item);
+            DB.SaveChanges();
+
+            return Ok(new Model_Result());
+        }
+
+        protected override bool MissingRight(UserRight right) => throw new NotImplementedException();
+    }
+}
