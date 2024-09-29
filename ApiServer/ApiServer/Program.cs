be83106b29c01735156600c74c8353bcbd216612
chloeeyline <88310367+chloeeyline@users.noreply.github.com>
2024-09-29 19:24:14 +0200

    Added Default Config fro Lambda

--- ApiServer/ApiServer/Program.cs
+++ ApiServer/ApiServer/Program.cs
@@ -7,48 +7,58 @@ using StyleWerk.NBB.AWS;
 using StyleWerk.NBB.Database;
 using StyleWerk.NBB.Helper;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-SecretData secretData = builder.AddAmazonSecretsManager();
-
-builder.Services.AddControllers(options =>
+try
 {
-    options.UseDateOnlyTimeOnlyStringConverters();
-    options.Filters.Add(new ProducesAttribute("application/json"));
-}).AddJsonOptions(options =>
+
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+    SecretData secretData = builder.AddAmazonSecretsManager();
+
+    builder.Services.AddControllers(options =>
+    {
+        options.UseDateOnlyTimeOnlyStringConverters();
+        options.Filters.Add(new ProducesAttribute("application/json"));
+    }).AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+    builder.Services.AddDbContext<NbbContext>(options => options.UseNpgsql(secretData.GetConnectionString()));
+    builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+    builder.AddJWT(secretData);
+    builder.AddSwagger();
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(options =>
+            options.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+    });
+
+    builder.Logging.SetMinimumLevel(LogLevel.Information);
+    builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning);
+    builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.None);
+
+    WebApplication app = builder.Build();
+
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseRouting();
+    app.UseCors();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.Run();
+}
+catch (Exception e)
 {
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-
-builder.Services.AddDbContext<NbbContext>(options => options.UseNpgsql(secretData.GetConnectionString()));
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
-
-builder.AddJWT(secretData);
-builder.AddSwagger();
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(options =>
-        options.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader());
-});
-
-builder.Logging.SetMinimumLevel(LogLevel.Information);
-builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning);
-builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.None);
-
-WebApplication app = builder.Build();
-
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseRouting();
-app.UseCors();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.Run();
+    Console.WriteLine(e.Message);
+    Exception? ex = e.InnerException;
+    while (ex != null) Console.WriteLine(ex.Message);
+}
 
