ff9aa733682d592f1c75a4ff8bf9b76e3c0a8dc2
schwammal666 <schwammal55@gmail.com>
2024-09-29 15:58:16 +0200

    Adding User tests

---
+++ ApiServer/ApiServer/Tests/UserTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace StyleWerk.NBB.Tests
+{
+    public class UserTest
+    {
+        [Fact]
+        public void Register()
+        {
+
+        }
+
+        [Fact]
+        public void Login()
+        {
+
+        }
+
+        [Fact]
+        public void ChangeEmail()
+        {
+
+        }
+
+        [Fact]
+        public void ChangePassword()
+        {
+
+        }
+    }
+}
