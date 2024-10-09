17da823b9c09b615cd813fefebb6318748ab9961
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-10-09 14:56:08 +0200

    Removed unused Props

--- ApiServer/ApiServer/Models/Share.cs
+++ ApiServer/ApiServer/Models/Share.cs
@@ -5,5 +5,5 @@ namespace StyleWerk.NBB.Models;
 public record Model_ShareItem(Guid ID, Guid ItemID, string WhoShared, ShareVisibility Visibility, ShareType Type, string? ToWhom);
 
 public record Model_Group(Guid ID, string Name, int UserCount);
-public record Model_GroupUser(string Username, Guid GroupID, bool CanAddUsers, bool CanRemoveUsers, string? WhoAdded);
+public record Model_GroupUser(string Username, Guid GroupID, string? WhoAdded);
 public record Model_SharedToGroup(Guid ID, ShareType Type, string Name);
