f718a69f23677e30114668defd821e92684a70cb
schwammal666 <schwammal55@gmail.com>
2024-10-14 12:20:19 +0200

    adjust filter tests

--- ApiServer/ApiServerTest/Tests/FilterEntryTest.cs
+++ ApiServer/ApiServerTest/Tests/FilterEntryTest.cs
@@ -27,55 +27,6 @@ namespace ApiServerTest.Tests
         }
 
         [Fact]
-        public void Filter_Public()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntry", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entrys = query.List(null, null, null, null, true);
-            Assert.Empty(entrys);
-        }
-
-        [Fact]
-        public void Filter_Shared()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntry", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entrys = query.List(null, null, null, null, false);
-            Assert.Empty(entrys);
-        }
-
-        [Fact]
-        public void Filter_Direct()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntry", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entrys = query.List(null, null, null, null, false);
-            Assert.True(entrys.Count > 0);
-        }
-
-        [Fact]
-        public void Filter_ShareTypes()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntry", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entrys = query.List(null, null, null, null, true);
-            Assert.NotNull(entrys);
-            Assert.True(entrys.Count > 0);
-        }
-
-        [Fact]
         public void Filter_Owned_Public()
         {
             Helpers.DeleteAll();
@@ -103,7 +54,7 @@ namespace ApiServerTest.Tests
         }
 
         [Fact]
-        public void Filter_EntryName_Public()
+        public void Filter_EntryName_Owned_Public()
         {
             Helpers.DeleteAll();
             DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
@@ -111,44 +62,9 @@ namespace ApiServerTest.Tests
             _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
             EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
             List<Model_EntryItem> entries = query.List("Test", null, null, null, true);
-            Assert.Empty(entries);
+            Assert.NotEmpty(entries);
         }
 
-        [Fact]
-        public void Filter_EntryName_Shared()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entries = query.List("Test", null, null, null, false);
-            Assert.Empty(entries);
-        }
-
-        [Fact]
-        public void Filter_EntryName_Direct()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entries = query.List("Test", null, null, null, false);
-            Assert.True(entries.Count > 0);
-        }
-
-        [Fact]
-        public void Filter_EntryName_ShareTypes()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entries = query.List("Test", null, null, null, true);
-            Assert.True(entries.Count > 0);
-        }
         #endregion
 
         #region Filter UserName ShareTypes
@@ -165,7 +81,7 @@ namespace ApiServerTest.Tests
         }
 
         [Fact]
-        public void Filter_UserName_Public()
+        public void Filter_UserName_Owned_Public()
         {
             Helpers.DeleteAll();
             DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
@@ -173,43 +89,7 @@ namespace ApiServerTest.Tests
             _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
             EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
             List<Model_EntryItem> entries = query.List(null, "Test", null, null, true);
-            Assert.Empty(entries);
-        }
-
-        [Fact]
-        public void Filter_UserName_Shared()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entries = query.List(null, "Test", null, null, false);
-            Assert.Empty(entries);
-        }
-
-        [Fact]
-        public void Filter_UserName_Direct()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entries = query.List(null, "Test", null, null, false);
-            Assert.Empty(entries);
-        }
-
-        [Fact]
-        public void Filter_UserName_ShareTypes()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> entries = query.List(null, "Test", null, null, true);
-            Assert.True(entries.Count > 0);
+            Assert.NotEmpty(entries);
         }
         #endregion
 
@@ -228,7 +108,7 @@ namespace ApiServerTest.Tests
         }
 
         [Fact]
-        public void Filter_TemplateName_Public()
+        public void Filter_TemplateName_Owned_Public()
         {
             Helpers.DeleteAll();
             DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
@@ -236,43 +116,7 @@ namespace ApiServerTest.Tests
             _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
             EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
             List<Model_EntryItem> templates = query.List(null, null, "Test", null, true);
-            Assert.Empty(templates);
-        }
-
-        [Fact]
-        public void Filter_TemplateName_Shared()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, "Test", null, false);
-            Assert.Empty(templates);
-        }
-
-        [Fact]
-        public void Filter_TemplateName_Direct()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, "Test", null, false);
-            Assert.True(templates.Count > 0);
-        }
-
-        [Fact]
-        public void Filter_TemplateName_ShareTypes()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, "Test", null, true);
-            Assert.True(templates.Count > 0);
+            Assert.NotEmpty(templates);
         }
         #endregion
 
@@ -290,54 +134,6 @@ namespace ApiServerTest.Tests
         }
 
         [Fact]
-        public void Filter_Tags_Public()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, null, "Test", true);
-            Assert.Empty(templates);
-        }
-
-        [Fact]
-        public void Filter_Tags_Shared()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, null, "Test", false);
-            Assert.Empty(templates);
-        }
-
-        [Fact]
-        public void Filter_Tags_Direct()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, null, "Test", false);
-            Assert.True(templates.Count > 0);
-        }
-
-        [Fact]
-        public void Filter_Tags_ShareTypes()
-        {
-            Helpers.DeleteAll();
-            DefaultUserGuid = Helpers.CreateUser(Username, Email, Password);
-            Model_Editor template = Helpers.CreateTemplate("TestTemplate", DefaultUserGuid.ToString(), null);
-            _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
-            EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
-            List<Model_EntryItem> templates = query.List(null, null, null, "Test", true);
-            Assert.True(templates.Count > 0);
-        }
-
-        [Fact]
         public void Filter_Tags_Owned_Public()
         {
             Helpers.DeleteAll();
@@ -346,7 +142,7 @@ namespace ApiServerTest.Tests
             _ = Helpers.CreateEntry(DefaultUserGuid.ToString(), "TestEntries", null, "Test", template);
             EntryQueries query = Helpers.ReturnEntryQuery(DefaultUserGuid.ToString());
             List<Model_EntryItem> templates = query.List(null, null, null, "Test", true);
-            Assert.True(templates.Count > 0);
+            Assert.NotEmpty(templates);
         }
         #endregion
 
