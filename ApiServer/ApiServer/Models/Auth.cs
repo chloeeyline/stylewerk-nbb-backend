416cce922a135012c75156076eb01c410904251f
schwammal666 <schwammal55@gmail.com>
2024-10-15 10:06:30 +0200

    adding comments to models

--- ApiServer/ApiServer/Models/Auth.cs
+++ ApiServer/ApiServer/Models/Auth.cs
@@ -1,22 +1,86 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-using StyleWerk.NBB.Database.User;
+﻿using StyleWerk.NBB.Database.User;
+using System.ComponentModel.DataAnnotations;
 
 namespace StyleWerk.NBB.Models;
 
+/// <summary>
+/// Used to get userdata of the current user
+/// </summary>
+/// <param name="Username">username of the current user</param>
+/// <param name="Email">email of the current user</param>
+/// <param name="FirstName">firstname of the current user</param>
+/// <param name="LastName">lastname of the current user</param>
+/// <param name="Gender">gender of the current user. Can be male, female or non binary</param>
+/// <param name="Birthday">birthday converted to unix timestamp</param>
 public record Model_UserData(string Username, string Email, string FirstName, string LastName, UserGender Gender, long Birthday);
+
+/// <summary>
+/// Used to update updateable userdata
+/// </summary>
+/// <param name="Password">new password or null if the password shouldn't be changed</param>
+/// <param name="FirstName">new firstname or null if the firstname shouldn't be changed</param>
+/// <param name="LastName">new lastname or null if the lastname shouldn't be changed</param>
+/// <param name="Gender">new gender or null if the gender shouldn't be changed</param>
 public record Model_UpdateUserData(string? Password, string? FirstName, string? LastName, UserGender? Gender);
 
+/// <summary>
+/// Used to register a new user
+/// </summary>
+/// <param name="Username">username of the new user</param>
+/// <param name="Email">email of the new user</param>
+/// <param name="Password">password of the new user</param>
+/// <param name="FirstName">firstname of the new user</param>
+/// <param name="LastName">lastname of the new user</param>
+/// <param name="Gender">gender of the new user</param>
+/// <param name="Birthday">birthday as unix timestamp</param>
 public record Model_Registration(string Username, string Email, string Password, string FirstName, string LastName, UserGender Gender, long Birthday);
 
+/// <summary>
+/// Used to login with the given parameters
+/// </summary>
+/// <param name="Username">username of the current user</param>
+/// <param name="Password">password of the current user</param>
+/// <param name="ConsistOverSession">decides whether the login will persist. Can be true or false</param>
 public record Model_Login([Required] string Username, [Required] string Password, bool ConsistOverSession);
+
+/// <summary>
+/// Used to login via refresh token
+/// </summary>
+/// <param name="Token">refresh token. Used to keep the user logged in</param>
+/// <param name="ConsistOverSession">decides whether the login should persist. Can be true or false</param>
 public record Model_RefreshToken(string Token, bool ConsistOverSession);
 
+/// <summary>
+/// Used to reset the current password of the current user
+/// </summary>
+/// <param name="Token">status token on password reset should be "PasswordReset"</param>
+/// <param name="Password">new password</param>
 public record Model_ResetPassword(string Token, string Password);
+
+/// <summary>
+/// Used to validate one of these options: username, password or email
+/// </summary>
+/// <param name="ToValidate">can be username, password or email as string</param>
 public record Model_ValidateIdentification(string ToValidate);
+
 public record Model_StatusToken(string Token);
 
 #region Result
+/// <summary>
+/// Will be returned on successful login
+/// </summary>
+/// <param name="AccessToken">access token as string. Used to identifiy the user and granting access to the application on login</param>
+/// <param name="RefreshToken">refresh token as string to keep the users login</param>
+/// <param name="StatusCode">status code as string. Can be one of the following: null, EmailVerification, EmailChange, PasswordReset</param>
+/// <param name="ConsistOverSession">decides whether the login should persist. Can be true or false</param>
+/// <param name="Username">username as string</param>
+/// <param name="Admin">shows if the current user is an admin. Can be true or false</param>
 public record AuthenticationResult(Model_Token AccessToken, Model_Token RefreshToken, UserStatus? StatusCode, bool ConsistOverSession, string Username, bool Admin);
+
+/// <summary>
+/// Used to get or set the refresh or access token
+/// </summary>
+/// <param name="Token">refresh or access token as string</param>
+/// <param name="ExpireTime">expire time of the token as long</param>
 public record Model_Token(string Token, long ExpireTime);
 #endregion
--- ApiServer/ApiServer/Models/Editor.cs
+++ ApiServer/ApiServer/Models/Editor.cs
@@ -1,5 +1,17 @@
 ﻿namespace StyleWerk.NBB.Models;
 
+/// <summary>
+/// Primary model for the whole editor. It's used to add a new template or entry as well as update these, show them in the editor or display them as non editable preview.
+/// </summary>
+/// <param name="ID">unique entry identifier</param>
+/// <param name="FolderID">unique identifier of the folder</param>
+/// <param name="TemplateID">unique template identifier</param>
+/// <param name="Name">name of the entry</param>
+/// <param name="Tags">tags on the entry</param>
+/// <param name="IsEncrypted">decides wether the entry is encrypted or not</param>
+/// <param name="IsPublic">decides if the entry can be shared public</param>
+/// <param name="Template">contains all template details</param>
+/// <param name="Items">list of all entry rows</param>
 public record Model_Editor(
     Guid ID,
     Guid? FolderID,
@@ -10,27 +22,70 @@ public record Model_Editor(
     bool IsPublic,
     Template? Template,
     List<EntryRow> Items);
+
+/// <summary>
+/// Represents one entry row
+/// </summary>
+/// <param name="ID">unique identifier of the row</param>
+/// <param name="TemplateID">unique identifier of the template the row is based on</param>
+/// <param name="Template">contains all template row details from the template the entry is based on</param>
+/// <param name="Items">list of all the entry cells within this row</param>
 public record EntryRow(
     Guid ID,
     Guid TemplateID,
     TemplateRow? Template,
     List<EntryCell> Items);
+
+/// <summary>
+/// Represents one entry cell within a row
+/// </summary>
+/// <param name="ID">unique identifier of the cell</param>
+/// <param name="TemplateID">uniuqe identifier of the template the cell is based on</param>
+/// <param name="Data">entry inputs from user</param>
+/// <param name="Template">contains all template cell details from the template the entry is based on</param>
 public record EntryCell(
     Guid ID,
     Guid TemplateID,
     string? Data,
     TemplateCell? Template);
 
+/// <summary>
+/// Illustrates the template without rows and cells
+/// </summary>
+/// <param name="ID">unique identifier of the template</param>
+/// <param name="Name">template name</param>
+/// <param name="IsPublic">boolean to show if the template is public visible</param>
+/// <param name="Description">description of the template</param>
+/// <param name="Tags">tags on the template</param>
 public record Template(
     Guid ID,
     string? Name,
     bool IsPublic,
     string? Description,
     string? Tags);
+
+/// <summary>
+/// Symbolizes one template row without being bound to the template
+/// </summary>
+/// <param name="ID">unique identifier of the template row</param>
+/// <param name="CanWrapCells">boolean to decide whether the row can wrap cells</param>
+/// <param name="CanRepeat">boolean to decide whether the row can be repeated</param>
+/// <param name="HideOnNoInput">boolean to decide whether the row should be shown if the input is null</param>
 public record TemplateRow(Guid ID,
     bool CanWrapCells,
     bool CanRepeat,
     bool HideOnNoInput);
+
+/// <summary>
+/// Symbolizes one template cell without being bound to the template row
+/// </summary>
+/// <param name="ID">unique identifier of the cell</param>
+/// <param name="InputHelper">specifies the datatype of the input</param>
+/// <param name="HideOnEmpty">boolean to decide whether the cell should be displayed if it's empty</param>
+/// <param name="IsRequired">boolean to decide whether the cell needs to have an input</param>
+/// <param name="Text">input of user</param>
+/// <param name="Description">describes the cell on mouse hover</param>
+/// <param name="MetaData">shows meta data of the cell</param>
 public record TemplateCell(
     Guid ID,
     int InputHelper,
